a20e326fafda86f0c2286bc737ce8e6d000e7143
Schuyler Ankele <schuyler.ankele@gmail.com>
2017-01-12 21:43:10 -0700

    Update buttonBuilder.js

--- buttonBuilder/buttonBuilder.js
+++ buttonBuilder/buttonBuilder.js
@@ -1,1 +1,8 @@
-
+//  Example call: buildButton('body', 'testClass', 'ok', 'Some Text');
+function buildButton(target, buttonClass, glyph, buttonText){
+        var button = document.createElement('button');
+        button.className = buttonClass;
+        button.innerHTML = buttonText;
+        button.innerHTML += '<span class="glyphicon glyphicon-' + glyph + '"></span>'; 
+        document.querySelector(target).appendChild(button);
+    }
